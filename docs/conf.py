57f34d099b1bb61aa1b56fee3247226ea25ead51
Mihir Mittal <105881639+mihirm21@users.noreply.github.com>
2023-05-30 19:24:29 +0530

    changes done
    Signed-off-by: Mihir Mittal <105881639+mihirm21@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'ofep-testing'
 copyright = '2023, Mihir Mittal'
@@ -16,6 +19,22 @@ release = '2'
 
 extensions = ["myst_parser",
   "sphinx_design",]
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
